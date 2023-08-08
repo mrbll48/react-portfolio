91cd8d94c2957835c0cd4b5ed800cdfbdd14165c
William Marks <william.marks87@gmail.com>
2023-08-08 15:58:58 -0700

    added starter code to portfolio.js, resume.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,19 +3,25 @@ import { Link } from "react-router-dom";
 
 export default function Header() {
   return (
-    <nav>
-      <Link to="/">
-        <button>Home</button>
-      </Link>
-      <Link to="/portfolio">
-        <button>Portfolio</button>
-      </Link>
-      <Link to="/contact">
-        <button>Contact</button>
-      </Link>
-      <Link to="/resume">
-        <button>Resume</button>
-      </Link>
-    </nav>
+    <div className="navbar navbar-expand-md navbar-dark bg-dark">
+      <nav className="navbare w-100 order-3 dual-collapse2">
+        <ul className="navbar-nav ml-auto">
+          <li className="nav-item">
+            <Link to="/">
+              <button class="">Home</button>
+            </Link>
+          </li>
+          <Link to="/portfolio">
+            <button>Portfolio</button>
+          </Link>
+          <Link to="/contact">
+            <button>Contact</button>
+          </Link>
+          <Link to="/resume">
+            <button>Resume</button>
+          </Link>
+        </ul>
+      </nav>
+    </div>
   );
 }
--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -1,3 +1,23 @@
 export default function Resume() {
-  return <div>test</div>;
+  return (
+    <div>
+      <a href="../../../Assets/WMarks-Resume.pdf" download>
+        <p>Download my resume</p>
+      </a>
+      <p>Front End Proficiencies</p>
+      <ul>
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>Javascript</li>
+        <li>Nodejs</li>
+        <li>React</li>
+      </ul>
+      <p>Back End Proficiencies</p>
+      <ul>
+        <li>MYSQL</li>
+        <li>NoSQL</li>
+        <li>GraphQL</li>
+      </ul>
+    </div>
+  );
 }
